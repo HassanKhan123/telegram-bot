4ba404df083e1d39f00f2d5585e438147b1d34dd
HassanKhan123 <hk2602809@gmail.com>
2021-01-16 18:51:23 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { Telegraf } = require('telegraf');
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
+
+app.listen(process.env.port || 5000, () => {
+  console.log('Server running');
+});
