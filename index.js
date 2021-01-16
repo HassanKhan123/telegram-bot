9503d72d66c21eae0dac044e261c83554a1be6ed
HassanKhan123 <hk2602809@gmail.com>
2021-01-16 19:17:57 +0500

    nodemon installed

--- index.js
+++ index.js
@@ -10,7 +10,25 @@ bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.command('oldschool', (ctx) => ctx.reply('Hello'));
+bot.command('hipster', Telegraf.reply('λ'));
+bot.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log('Response time: %sms', ms);
+});
+
+bot.on('text', (ctx) => ctx.reply('Hello World'));
+bot.catch((err, ctx) => {
+  console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
+});
+bot.start((ctx) => {
+  throw new Error('Example error');
+});
 bot.launch();
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server running');
